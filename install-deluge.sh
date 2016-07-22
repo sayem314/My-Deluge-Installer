58fec6a99af81904bbd1efaa4d7b227480ff6e70
sayem314 <sayem314@gmail.com>
2016-07-22 16:59:20 +0600

    Added support for uninstall

--- install-deluge.sh
+++ install-deluge.sh
@@ -9,6 +9,21 @@ ip=`wget -qO- ipv4.icanhazip.com`
 user=`whoami`
 nocert="--no-check-certificate"
 
+amiroot () {
+	echo ""
+	echo "  use -install or -del"
+	echo ""
+}
+
+amiroot () {
+	if [[ "$EUID" -ne 0 ]]; then
+		echo ""
+		echo "  Sorry, you need to run this as root"
+		exit
+	fi
+}
+
+installdeluge () {
 	if [[ -e /etc/systemd/system/deluge.service ]]; then
 		echo ""
 		echo "  Deluge is already installed"
@@ -21,30 +36,56 @@ nocert="--no-check-certificate"
 		exit
 	fi
 
-	if [[ "$EUID" -ne 0 ]]; then
-		echo ""
-		echo "  Sorry, you need to run this as root"
-		exit
-	fi
-
 	echo ""
-	echo "Installing deluge"
+	echo "  Installing deluge"
 	apt-get update -qy &>/dev/null && apt-get install deluged deluge-webui -qy &>/dev/null
-	echo "Deluge Installed"
+	echo "  Deluge Installed"
 	
 	# Creating Service
 	if [ "$init" == 'systemd' ]; then
-	rm /etc/systemd/system/deluge.service &>/dev/null
-	wget -q $nocert "https://raw.githubusercontent.com/sayem314/My-Deluge-Installer/master/etc/deluge.service" -O "/etc/systemd/system/deluge.service" &>/dev/null
+	rm /etc/systemd/system/deluge.service
+	wget $nocert "https://raw.githubusercontent.com/sayem314/My-Deluge-Installer/master/etc/deluge.service" -O "/etc/systemd/system/deluge.service"
 	systemctl enable deluge
 	elif [ "$init" == 'init' ]; then
-	rm /etc/init/deluge.conf &>/dev/null
-	wget -q $nocert "https://raw.githubusercontent.com/sayem314/My-Deluge-Installer/master/etc/deluge.conf" -O "/etc/init/deluge.conf" &>/dev/null
+	rm /etc/init/deluge.conf
+	wget $nocert "https://raw.githubusercontent.com/sayem314/My-Deluge-Installer/master/etc/deluge.conf" -O "/etc/init/deluge.conf"
 	fi
-	echo ""
 	service deluge start
 	echo ""
-	echo "Access deluge at $(tput setaf 3)http://$ip:8112$(tput sgr0)"
-	echo "Default deluge password is $(tput setaf 3)deluge$(tput sgr0)"
+	echo "  Access deluge at $(tput setaf 3)http://$ip:8112$(tput sgr0)"
+	echo "  Default deluge password is $(tput setaf 3)deluge$(tput sgr0)"
 	echo ""
 	exit
+}
+
+uninstalldeluge () {
+	# Deleting Service
+	echo ""
+	echo "  Stopping deluge process"
+	service deluge stop
+	if [ "$init" == 'systemd' ]; then
+	systemctl disable deluge
+	rm /etc/systemd/system/deluge.service
+	elif [ "$init" == 'init' ]; then
+	rm /etc/init/deluge.conf
+	fi
+	echo ""
+	echo "  Uninstalling deluge"
+	apt-get purge deluged deluge-webui -qy &>/dev/null
+	apt-get autoremove &>/dev/null
+	apt-get autoclean &>/dev/null
+	echo "  Deluge Uninstalled"
+	echo ""
+	exit
+}
+
+# See how we were called.
+case $1 in
+	'-install'|'install' )
+		amiroot; installdeluge;;
+	'-del'|'delete'|'-rm'|'-uninstall'|'uninstall' )
+		amiroot; uninstalldeluge;;
+	*)
+		error;;
+esac
+exit 1
