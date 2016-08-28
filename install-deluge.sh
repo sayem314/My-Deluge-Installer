51f3348e8ca9aa248c5196d6dca60e4c3c1cf3d9
Sayem Chowdhury <sayem314@gmail.com>
2016-08-28 12:40:27 +0600

    Fixed lots of issue

--- install-deluge.sh
+++ install-deluge.sh
@@ -8,118 +8,114 @@ init=`cat /proc/1/comm`
 ip=`wget -qO- ipv4.icanhazip.com`
 user=`whoami`
 nocert="--no-check-certificate"
+bashname=$(basename $BASH_SOURCE)
 
 howto () {
 	echo ""
-	echo "  use -install or -del"
+	echo " To install, run"
+	echo " $bashname -install"
+	echo ""
+	echo " To uninstall, run"
+	echo " $bashname -uninstall"
 	echo ""
 }
 
 chksudo () {
 	if [[ "$EUID" -ne 0 ]]; then
 		echo ""
-		echo "  Sorry, you need to run this as root"
+		echo " Sorry, you need to run this as root"
 		echo ""
 		exit
 	fi
 }
 
 installdeluge () {
-	if [[ -e /etc/systemd/system/deluge.service ]]; then
+	if [[ -e /etc/systemd/system/deluge.service || -e /etc/init/deluge.conf|| -e /etc/default/deluge-daemon ]]; then
 		echo ""
-		echo "  Deluge is already installed"
-		echo ""
-		exit
-	elif [[ -e /etc/init/deluge.conf ]]; then
-		echo ""
-		echo "  Deluge is already installed"
+		echo " Deluge is already installed"
 		echo ""
 		exit
 	fi
 
 	echo ""
-	echo -n "  Installing deluge"
+	echo -n " Installing python-software-properties..."
 	apt-get install python-software-properties -y &>/dev/null
+	echo "  $(tput setaf 2)DONE$(tput sgr0)"
+	echo -n " Addning repository ppa:deluge-team/ppa..."
 	yes ENTER | add-apt-repository ppa:deluge-team/ppa &>/dev/null
+	echo "  $(tput setaf 2)DONE$(tput sgr0)"
+	echo -n " Upadting repository..."
 	apt-get update -y &>/dev/null
+	echo "  $(tput setaf 2)DONE$(tput sgr0)"
+	echo -n " Installing deluged deluge-webui..."
 	apt-get install deluged deluge-webui -y &>/dev/null
 	echo "  $(tput setaf 2)DONE$(tput sgr0)"
+	echo ""
 	
 	# Starting Service
-	deluged
-	deluge-web --fork
+	makeservice;
+	#deluged
+	#deluge-web --fork
 	echo ""
-	echo "  Access deluge at $(tput setaf 3)http://$ip:8112$(tput sgr0)"
-	echo "  Default deluge password is $(tput setaf 3)deluge$(tput sgr0)"
+	invoke-rc.d deluge-daemon start
+	echo ""
+	echo " Access deluge at $(tput setaf 3)http://$ip:8112$(tput sgr0)"
+	echo " Default deluge password is $(tput setaf 3)deluge$(tput sgr0)"
 	echo ""
 
 }
 
 makeservice () {
-	echo "  Not implented yet"
-	exit;
-	echo -n "  Creating service..."
-	if [ "$init" == 'systemd' ]; then
-		rm -f /etc/systemd/system/deluge.service
-		cat <<EOF > /etc/systemd/system/deluge.service
-[Unit]
-Description=Deluge Bittorrent Client Web Interface
-After=network-online.target
-
-[Service]
-Type=simple
-User=$user
-Group=$user
-UMask=027
-ExecStart=/usr/bin/deluge-web
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
+	rm -f /etc/default/deluge-daemon
+	cat <<EOF > /etc/default/deluge-daemon
+# Configuration for /etc/init.d/deluge-daemon
+# The init.d script will only run if this variable non-empty.
+DELUGED_USER="root"
+# Should we run at startup?
+RUN_AT_STARTUP="YES"
 EOF
-		chmod 0644 /etc/systemd/system/deluge.service
-		echo "  $(tput setaf 2)DONE$(tput sgr0)"
-		systemctl enable deluge
-	elif [[ "$init" == 'init' ]]; then
-		rm -f /etc/init/deluge.conf
-		cat <<EOF > /etc/init/deluge.conf
-start on started deluge
-stop on stopping deluge
-
-env uid=$user
-env gid=$user
-env umask=027
-
-exec start-stop-daemon -S -c $uid:$gid -k $umask -x /usr/bin/deluge-web
-EOF
-		echo "  $(tput setaf 2)DONE$(tput sgr0)"
-	else
-		echo "  $(tput setaf 1)FAILED$(tput sgr0)"
-
-	fi
+	wget -q $nocert https://raw.githubusercontent.com/sayem314/My-Deluge-Installer/master/etc/deluge-daemon -O /etc/init.d/deluge-daemon
+	chmod +x /etc/init.d/deluge-daemon
+	update-rc.d deluge-daemon defaults
 }
 
 uninstalldeluge () {
-	# Deleting Service
-	echo ""
-	echo "  Stopping deluge process"
-	service deluged stop
-	killall deluged
-	killall deluge-web
-	if [ "$init" == 'systemd' ]; then
-	systemctl disable deluge
-	rm -f /etc/systemd/system/deluge.service
-	elif [ "$init" == 'init' ]; then
-	rm -f /etc/init/deluge.conf
+	if [[ -e /etc/systemd/system/deluge.service || -e /etc/init/deluge.conf|| -e /etc/default/deluge-daemon ]]; then
+		echo ""
+		echo " Are you sure you want to uninstall Deluge? [y/N]"
+		read -p " Select an option: " option
+		case $option in
+			[yY][eE][sS]|[yY])
+			echo ""
+			echo " Stopping deluge process"
+			killall deluged
+			killall deluge-web
+			if [ "$init" == 'systemd' ]; then
+			systemctl disable deluge
+			rm -f /etc/systemd/system/deluge.service
+			elif [ "$init" == 'init' ]; then
+			rm -f /etc/init/deluge.conf
+			fi
+			echo ""
+			echo " Uninstalling deluge"
+			rm -f /etc/default/deluge-daemon
+			rm -f /etc/init.d/deluge-daemon
+			update-rc.d -f deluge-daemon remove
+			apt-get remove --purge deluged deluge-webui -y &>/dev/null
+			apt-get autoremove -y &>/dev/null
+			apt-get autoclean -y &>/dev/null
+			echo " Deluge Uninstalled"
+			echo ""
+			exit
+			;;
+			[nN][oO]|[nN]) echo ""; exit;;
+			*) echo ""; echo " Incorrect input, exiting! "; echo "";;
+		esac
+		else
+		echo ""
+		echo "  Looks like Deluge is not installed"
+		echo ""
 	fi
-	echo ""
-	echo "  Uninstalling deluge"
-	apt-get remove --purge deluged deluge-webui -y &>/dev/null
-	apt-get autoremove -y &>/dev/null
-	apt-get autoclean -y &>/dev/null
-	echo "  Deluge Uninstalled"
-	echo ""
-	exit
 }
 
 # See how we were called.
